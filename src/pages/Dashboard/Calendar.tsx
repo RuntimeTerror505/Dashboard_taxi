4b26082cc483057691679f9508e8b6db58d4b572
Ihor Kachynskyi <ikachynskiy@gmail.com>
2023-12-15 18:37:42 +0200

    new commit

--- src/pages/Dashboard/Calendar.tsx
+++ src/pages/Dashboard/Calendar.tsx
@@ -1,44 +1,41 @@
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import 'react-date-range/dist/styles.css'; // main css file
 import { Select, Input } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GoSearch } from "react-icons/go";
 import { DateRange } from 'react-date-range';
 import { addDays } from 'date-fns';
 import { Calendar as CalendarContent, dayjsLocalizer } from 'react-big-calendar'
 import dayjs from 'dayjs'
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import '../../index.css'
+import myEventsList from './db' 
 
 const localizer = dayjsLocalizer(dayjs)
 
+interface FilterType {
+    type: 'time' |'date' |'name' |'phone' | 'email',
+    value:string,
+}
 const Calendar = (): React.ReactNode => {
     const [range, setRange] = useState(1)
-    const [state, setState] = useState({
+    const [ timeRange, setTimeRange ] = useState({
         selection: {
-            startDate: new Date(),
-            endDate: new Date(),
+            startDate: dayjs().toDate(),
+            endDate: dayjs().toDate(),
             key: 'selection'
-        },
-        compare: {
-            startDate: new Date(),
-            endDate: new Date(),
-            key: 'compare'
         }
     });
-
-    const myEventsList:{id?:number,title:string,allDay?:boolean,start:Date,end:Date}[] =[
-        { 
-            
-            title: "title",
-            start: dayjs().set('hour', 7).set('minute',15).toDate(),
-            end: dayjs().set('hour', 9).set('minute',30).toDate(), 
+    const [filter,setFilter]= useState<FilterType>({type:'date', value:''})
+    const [calendarDate, setCalendarDate] = useState(dayjs().toDate())
+    const [events, setEvents] = useState(myEventsList)
+    const [trigger, setTrigger] = useState(false)
+    useEffect(()=>{
+        if(trigger) {
+            const res = myEventsList.filter(item=> (item.start > timeRange.selection.startDate && item.end < timeRange.selection.endDate))
+            setEvents(res)
         }
-    ]
+    },[timeRange])
 
-    const setFilterType = () => {
-
-    }
     const options = ['time', 'date', 'name', 'phone']
     // const monthsNames = [
     //     "January",
@@ -58,6 +55,15 @@ const Calendar = (): React.ReactNode => {
     var months = Array.from({ length: 12 }, (_, index) => index + 1);
     var years = Array.from({ length: 50 }, (_, index) => index + 2001);
 
+    const handleEvent =(event:any) =>{
+        console.log(event)
+    }
+    const handleClickEvent = (data:string) => {
+        const res = dayjs(data).toDate()
+        setCalendarDate(res)
+    }
+
+
     return (
         <div className={container}>
 {/*_________________________________________________MENU_______________________________________________________________________________   */}
@@ -72,12 +78,29 @@ const Calendar = (): React.ReactNode => {
 
                 {/* ______________ TABS _________________________________________ */}
                 <div className={tabs}>
-                    <div className={range === 1 ? menuTabActive : menuTab} onClick={() => setRange(1)}>Range dates</div>
-                    <div className={range === 2 ? menuTabActive : menuTab} onClick={() => setRange(2)}>Last 7 days</div>
-                    <div className={range === 3 ? menuTabActive : menuTab} onClick={() => setRange(3)}>Yesterday</div>
-                    <div className={range === 4 ? menuTabActive : menuTab} onClick={() => setRange(4)}>Today</div>
-                    <div className={range === 5 ? menuTabActive : menuTab} onClick={() => setRange(5)}>Tomorrow</div>
-                    <div className={range === 6 ? menuTabActive : menuTab} onClick={() => setRange(6)}>Next 7 days</div>
+                    <div className={range === 1 ? menuTabActive : menuTab} onClick={() => {
+                            setRange(1)
+                        }}>Range dates</div>
+                    <div className={range === 2 ? menuTabActive : menuTab} onClick={() => {
+                            setCalendarDate(dayjs().subtract(1, 'week').toDate())
+                            setRange(2)
+                        }}>Last 7 days</div>
+                    <div className={range === 3 ? menuTabActive : menuTab} onClick={() => {
+                            setCalendarDate(dayjs().subtract(1, 'day').toDate())
+                            setRange(3)
+                        }}>Yesterday</div>
+                    <div className={range === 4 ? menuTabActive : menuTab} onClick={() => {
+                            setCalendarDate(dayjs().toDate())
+                            setRange(4)
+                        }}>Today</div>
+                    <div className={range === 5 ? menuTabActive : menuTab} onClick={() => {
+                            setCalendarDate(dayjs().add(1, 'day').toDate())
+                            setRange(5)
+                        }}>Tomorrow</div>
+                    <div className={range === 6 ? menuTabActive : menuTab} onClick={() => {
+                            setCalendarDate(dayjs().add(1, 'week').toDate())
+                            setRange(6)
+                        }}>Next 7 days</div>
                 </div>
                 {/* _______________FILTERS________________________________________ */}
 
@@ -87,13 +110,16 @@ const Calendar = (): React.ReactNode => {
                         <Select
                             className=' filterSelect '
                             placeholder="select filter"
+                            value={filter.type}
                             style={{ width: '40%' }}
-                            onChange={setFilterType}
+                            onChange={(e)=>setFilter({...filter, type:e})}
                             options={options.map(item => (
                                 { value: item, label: item }
                             ))}
                         />
                         <Input
+                            value={filter.value}
+                            onChange={(e)=>setFilter({...filter, value:e.target.value})}
                             className='w-1/2 rounded-r-full'
                             placeholder='Find your destiny'
                         />
@@ -108,22 +134,46 @@ const Calendar = (): React.ReactNode => {
                 {/* ______________________________PICKER_______________________________________ */}
                 <div className={calendarSection}>
                     <DateRange
-                        onChange={item => setState({ ...state, ...item })}
+                        onChange={item => {
+                            setTrigger(true)
+                            setTimeRange({ ...timeRange, ...item })
+                        }}
                         months={1}
-                        minDate={addDays(new Date(), -100)}
-                        maxDate={addDays(new Date(), 900)}
-                        ranges={[state.selection, state.compare]}
+                        minDate={addDays(new Date(), -5000)}
+                        maxDate={addDays(new Date(), 2300)}
+                        ranges={[timeRange.selection]}
                     />
+                    <div className={eventList}>
+                        <h1 
+                            className='ml-auto mb-2 cursor-pointer text-rose-500'
+                            onClick={()=>{
+                                setTrigger(false)
+                                setEvents(myEventsList)
+                            }}
+                        >clear filters</h1>
+                        {events
+                            .filter(item => item.data[filter.type].toLowerCase().includes(filter.value.toLowerCase()) )
+                            .map(item=>(
+                            <div 
+                                onClick={()=> handleClickEvent(item.data.date)}
+                                key={item.title} 
+                                className={listItem}
+                            >{item.data.name}, {item.data.email}</div>
+                        ))}
+                    </div>
                 </div>
 
                 {/* ______________________________CALENDAR_______________________________________ */}
                 <div className={calendarContent}>
                     <CalendarContent
                         localizer={localizer}
-                        events={myEventsList}
+                        events={events}
                         startAccessor="start"
                         endAccessor="end"
                         style={{ height: 500 }}
+                        onSelectEvent={handleEvent}
+                        date={calendarDate}
+                        onNavigate={(event)=> setCalendarDate(dayjs(event).toDate())}
                     />
                 </div>
             </div>
@@ -133,8 +183,10 @@ const Calendar = (): React.ReactNode => {
 
 export default Calendar;
 
+const listItem = ' border-b hover:bg-purple-500 hover:text-white cursor-pointer px-2 py-1 rounded-full '
+const eventList = ' flex flex-col bg-white shadow-lg rounded-lg m-2 mr-0 p-2'
 const calendarContent = 'w-full p-6 bg-white rounded-xl shadow-xl m-6 '
-const calendarSection = ''
+const calendarSection = 'flex flex-col'
 
 const content = 'flex'
 
